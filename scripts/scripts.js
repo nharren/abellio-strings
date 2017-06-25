a071117de6163120d7338a61edb9fa0713bf2b19
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-25 01:06:03 -0700

    Add basic sorting.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,6 +17,7 @@ view.handleMenuEvents = function() {
     selectedSection.fadeIn(100);
 
     if (selectedSectionName === 'music' && !view.hasBuiltMusicTable) {
+      console.log($('.music-table tr'));
       view.buildMusicTable();
     }
 
@@ -25,11 +26,12 @@ view.handleMenuEvents = function() {
 }
 
 view.buildMusicTable = function() {
-  let source = $(".music-template").html();
+  let source = $(".music-template").html().trim();
   let template = Handlebars.compile(source);
-  let table = $('.music-table');
+  let table = $('.music-table tbody');
 
   $.getJSON('data/music.json', function(data) {
+    console.log('data length: ' + data.length);
     for (let i = 0; i < data.length; i++) {
       let element = data[i];
       let musicRow = template(element);
@@ -37,6 +39,32 @@ view.buildMusicTable = function() {
     }
   });
   
+  view.handleMusicTableHeaders();
+}
+
+view.handleMusicTableHeaders = function() {
+  $('.music-table-title-header').on('click', function(event) {
+    view.sortMusicTableByColumn(0);
+    event.preventDefault();
+  });
+
+  $('.music-table-composer-header').on('click', function(event) {
+    view.sortMusicTableByColumn(1);
+    event.preventDefault();
+  });
+
+  $('.music-table-genre-header').on('click', function(event) {
+    view.sortMusicTableByColumn(2);
+    event.preventDefault();
+  });
+}
+
+view.sortMusicTableByColumn = function(column) {
+  let sortedRows = $('.music-row').detach().sort(function(a,b) {
+    return a.children[column].innerText.localeCompare(b.children[column].innerText);
+  });
+  console.log('rows length: ' + sortedRows.length);
+  sortedRows.appendTo('.music-table tbody');
 }
 
 $(document).ready(function() {
