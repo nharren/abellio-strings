3b63b2f9a37cf9d31c6164f46996326d66089635
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-24 23:56:28 -0700

    Load music data into table with handlebars template.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,7 @@
 let view = {};
 
+view.hasBuiltMusicTable = false;
+
 view.handleMenuEvents = function() {
   $('.menu-item').on('click', function(event) {
     $('section').hide();
@@ -14,8 +16,27 @@ view.handleMenuEvents = function() {
     selectedSection.removeClass('hidden');
     selectedSection.fadeIn(100);
 
+    if (selectedSectionName === 'music' && !view.hasBuiltMusicTable) {
+      view.buildMusicTable();
+    }
+
     event.preventDefault();
   });
+}
+
+view.buildMusicTable = function() {
+  let source = $(".music-template").html();
+  let template = Handlebars.compile(source);
+  let table = $('.music-table');
+
+  $.getJSON('data/music.json', function(data) {
+    for (let i = 0; i < data.length; i++) {
+      let element = data[i];
+      let musicRow = template(element);
+      table.append(musicRow);
+    }
+  });
+  
 }
 
 $(document).ready(function() {
