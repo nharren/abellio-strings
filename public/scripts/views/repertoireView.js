ffc5c2df63eb38d8e3bf3e5a03f31241c9a343be
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-07-13 15:46:52 -0700

    Adds transitions to filters.

--- public/scripts/views/repertoireView.js
+++ public/scripts/views/repertoireView.js
@@ -10,73 +10,115 @@ var app = app || {};
   repertoireView.initialize = function() {
     app.view.showSection('repertoire');
 
-    if (!repertoireView.initialized) {
-      loadData();
-      handleFilter('composer');
-      handleFilterItems('composer');
-      handleFilter('genre');
-      handleFilterItems('genre');
-      repertoireView.initialized = true;
+    if (repertoireView.initialized) {
+      return;
     }
+    
+    handleFilter('composer');
+    handleFilterItems('composer');
+    handleFilter('genre');
+    handleFilterItems('genre');
+
+    repertoireView.initialized = true;
+    
+    loadData(populateItems);
   }
-
-  function handleFilter(name) {
-    let selection = document.getElementById(`${name}-filter-selection`);
-    selection.addEventListener('mousedown', generateSelectionHandler(name));
-  }
-
-  function generateSelectionHandler(name) {
-    return function (event) {
-      this.style.display = 'none';
-      let filterDropdown = document.getElementById(`${name}-filter-dropdown`);
-      filterDropdown.style.display = 'block';
-      event.preventDefault();
-    };
-  }
-
-  function handleFilterItems(name) {
-    let dropdown = document.getElementById(`${name}-filter-dropdown`);
-    dropdown.addEventListener('mousedown', generateFilterItemHandler(name));
-  }
-
-  function generateFilterItemHandler(name) {
-    return function (event) {
-      let isFilterItem = event.target.classList.contains('filter-item');
-
-      if (!isFilterItem) {
-        return;
-      }
-      
-      let filterSelection = document.getElementById(`${name}-filter-selection`);
-      filterSelection.innerText = event.target.innerText;
-      filterSelection.style.display = 'block';
-
-      let filterDropdown = document.getElementById(`${name}-filter-dropdown`);
-      filterDropdown.style.display = 'none';
-
-      event.preventDefault();
-    };
-  }
-
-  function loadData() {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = processDataResponse;
+  
+  function loadData(callback) {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = processDataResponse.bind(request, callback);
     request.open('GET', '/data/music.json');
     request.send();
   }
 
-  function processDataResponse() {
+  function processDataResponse(callback, event) {
     if (this.readyState == 4 && this.status == 200) {
       let data = JSON.parse(this.responseText);
-      populateComposerFilter(data);
-      populateGenreFilter(data);
-      populateList(data);
+      callback(data);
     }
+  }
+
+  function populateItems(data) {
+    populateComposerFilter(data);
+    populateGenreFilter(data);
+    populateList(data);
+  }
+
+  function handleFilter(name) {
+    let selection = document.getElementById(`${name}-filter-selection`);
+    let selectionHandler = displayFilterDropdown.bind(selection, name);
+    selection.addEventListener('mousedown', selectionHandler);
+  }
+
+  function displayFilterDropdown(name, event) {
+    this.style.display = 'none';
+    let filterDropdown = document.getElementById(`${name}-filter-dropdown`);
+    filterDropdown.classList.add('open');
+    event.preventDefault();
+  }
+
+  function handleFilterItems(name) {
+    let dropdown = document.getElementById(`${name}-filter-dropdown`);
+    let filterItemHandler = selectFilterItem.bind(null, name);
+    dropdown.addEventListener('mousedown', filterItemHandler);
+  }
+
+  function selectFilterItem(name, event) {
+    let filterItem = event.target;
+    let correctType = isFilterItem(filterItem);
+
+    if (!correctType) {
+      return;
+    }
+
+    let filterSelection = document.getElementById(`${name}-filter-selection`);
+    filterSelection.innerText = event.target.innerText;
+    filterSelection.style.display = 'block';
+
+    let filterDropdown = document.getElementById(`${name}-filter-dropdown`);
+    filterDropdown.classList.remove('open');
+
+    let repertoireList = document.getElementById('repertoire-list');
+    repertoireList.innerHTML = '';
+
+    let callback = filterBy.bind(null, name, filterItem.innerHTML);
+    loadData(callback);
+
+    event.preventDefault();
+  }
+
+  function isFilterItem(element) {
+    return element.classList.contains('filter-item');
+  }
+
+  function filterBy(name, value, dataItems) {
+    let filteredData = [];
+
+    for (let i = 0; i < dataItems.length; i++) {
+      let dataItem = dataItems[i];
+
+      if (name === 'composer') {
+        let match = dataItem.composer === value;
+
+        if (match) {
+          filteredData.push(dataItem);
+        }
+      } else if (name === 'genre') {
+        let match = dataItem.genres.includes(value);
+
+        if (match) {
+          filteredData.push(dataItem);
+        }
+      }
+    }
+
+    populateList(filteredData);
   }
 
   function populateComposerFilter(data) {
     data.sort(sortByComposer);
-    data = getDistinctComposers(data);
+    data = data.map(getComposer);
+    data = getDistinct(data);
     let toHtml = compileTemplate('composer-filter-item-template');
     let itemHtmlArray = data.map(toHtml);
     let list = document.getElementById('composer-filter-dropdown');
@@ -88,55 +130,29 @@ var app = app || {};
     return a.composer.localeCompare(b.composer);
   }
 
-  function getDistinctComposers(data) {
-    let distinctComposers = {};
-    
-    for (let i = 0; i < data.length; i++) {
-      let item = data[i];
-      let composer = item.composer;
-      let distinctComposer = distinctComposers[composer];
+  function getComposer(dataItem) {
+    return dataItem.composer;
+  }
 
-      if (distinctComposer) {
-        distinctComposer.count++;
-      } else {
-        distinctComposer = {};
-        distinctComposer.name = composer;
-        distinctComposer.count = 1;
-        distinctComposers[composer] = distinctComposer;
-      }
+  function getDistinct(items) {
+    let distinctItems = {};
+    
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+      distinctItems[item] = item;
     }
 
-    return Object.values(distinctComposers);
+    return Object.values(distinctItems);
   }
 
   function populateGenreFilter(data) {
-    data = getDistinctGenres(data);
+    data = getGenres(data);
+    data = getDistinct(data);
     let toHtml = compileTemplate('genre-filter-item-template');
     let itemHtmlArray = data.map(toHtml);
     let list = document.getElementById('genre-filter-dropdown');
     let itemsHtml = itemHtmlArray.join('');
     list.insertAdjacentHTML('beforeend', itemsHtml);
-  }
-
-  function getDistinctGenres(data) {
-    let genres = getGenres(data);
-    let distinctGenres = {};
-    
-    for (let genreIndex = 0; genreIndex < genres.length; genreIndex++) {
-      let genre = genres[genreIndex];
-      let distinctGenre = distinctGenres[genre];
-
-      if (distinctGenre) {
-        distinctGenre.count++;
-      } else {
-        distinctGenre = {};
-        distinctGenre.name = genre;
-        distinctGenre.count = 1;
-        distinctGenres[genre] = distinctGenre;
-      }
-    }
-
-    return Object.values(distinctGenres);
   }
 
   function getGenres(data) {
@@ -152,6 +168,7 @@ var app = app || {};
 
   function populateList(data) {
     data.sort(sortByTitle);
+
     let toHtml = compileTemplate('repertoire-item-template');
     let itemHtmlArray = data.map(toHtml);
     let list = document.getElementById('repertoire-list');
