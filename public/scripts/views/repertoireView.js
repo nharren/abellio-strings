87e00c966505fc2715feae220b5d7c9c6638a1cc
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-07-26 09:10:56 -0700

    Moved social.

--- public/scripts/views/repertoireView.js
+++ public/scripts/views/repertoireView.js
@@ -78,7 +78,7 @@ var app = app || {};
     request.send();
   }
 
-  function processDataResponse(callback, event) {
+  function processDataResponse(callback) {
     if (this.readyState == 4 && this.status == 200) {
       data = JSON.parse(this.responseText);
       callback(data);
